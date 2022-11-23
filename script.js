401c82e92587ea653ecfb3673f00e7d59d201d55
nickrosales <116924957+nickrosales@users.noreply.github.com>
2022-11-22 20:11:51 -0700

    added funtion that get password length, added fuction to get boolean values for other user input. added a function to return a rnadom number based on an array. added a fuunction to generate a password based on the answers givin

--- script.js
+++ script.js
@@ -1,5 +1,66 @@
 // Assignment Code
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
 var generateBtn = document.querySelector("#generate");
+function getNumber () {
+  var passLengthNum = prompt("How many characters do you want your password to be? Enter a number between 8 and 128.");
+  if(passLengthNum >= 8 && passLengthNum <= 128) {
+      return passLengthNum
+  } else {
+    alert("Please enter a valid number");
+    return getNumber();
+      
+  }
+}
+
+function passwordAnswers() {
+  var passLengthNum = getNumber();
+  var userChoiceNum = confirm("Do you want numbers in your password?");
+  var lowerChoice = confirm("Do you want lower case letters in your password?");
+  var capsChoice = confirm("Do you want upper case letters in your password?");
+  var symChoice = confirm("Do you want symbols in your password?");
+  
+  var userInput = {
+      nums: userChoiceNum,
+      lowerCase: lowerChoice,
+      upper: capsChoice,
+      sym: symChoice,
+      passLength: passLengthNum
+  };
+  return userInput
+}
+
+function makeRandom(length) {
+  var randomPass = Math.floor(Math.random() * length);
+return randomPass
+}
+
+function generatePassword() {
+  var userInput = passwordAnswers();
+  var passwordChars = [];
+  var myPass = [];
+  if(userInput.nums) {
+    passwordChars = passwordChars.concat(numbers);
+  }
+  if(userInput.lowerCase) {
+    passwordChars = passwordChars.concat(lower);
+  }
+  if(userInput.upper) {
+    passwordChars = passwordChars.concat(caps);
+  }
+  if(userInput.sym) {
+    passwordChars = passwordChars.concat(symbols);
+  }
+
+
+
+  for(var i = 0; i < userInput.passLength; i++) {
+    myPass.push(passwordChars[makeRandom(passwordChars.length)])
+  }
+  return myPass.join("")
+}
 
 // Write password to the #password input
 function writePassword() {
